a5c8ea46c6163e177c96c74c510c8990bf3f459e
Valentine <valentine.nazarov@gmail.com>
2021-08-28 23:05:43 +0300

    refactor: 4-kyu-adding-big-numbers.js

--- 4-kyu-adding-big-numbers.js
+++ 4-kyu-adding-big-numbers.js
@@ -1,13 +1,12 @@
 function add(a, b) {
-    let result = '';
     if (a.length < b.length)[a, b] = [b, a];
-    const delta = a.length - b.length;
-    let overflow = 0;
+    let carry = 0;
+    let result = new Array(a.length + 1);
     for (let i = a.length - 1; i >= 0; i--) {
-        let singleDigitSum = parseInt(a[i]) + (parseInt(b[i - delta]) || 0) + overflow;
-        overflow = (singleDigitSum.toString().length > 1) ? 1 : 0;
-        singleDigitSum = (overflow) ? singleDigitSum % 10 : singleDigitSum;
-        result = singleDigitSum + result;
+        let singleDigitSum = parseInt(a[i]) + (parseInt(b[i - (a.length - b.length)]) || 0) + carry;
+        carry = ~~(singleDigitSum / 10);
+        result[i + 1] = singleDigitSum % 10;
     }
-    return (overflow || "") + result;
+    if (carry) result[0] = 1;
+    return result.join("");
 }
