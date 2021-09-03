822dd5007dbc2db0e3c01cb48c38ad1f0e66b570
Valentine <valentine.nazarov@gmail.com>
2021-09-03 10:45:08 +0300

    remove recursion, optimize performance: 3-kyu-prime-streaming.js

--- 3-kyu-prime-streaming.js
+++ 3-kyu-prime-streaming.js
@@ -2,25 +2,41 @@
 
 class Primes {
     static* stream() {
+        const primes = [];
         yield 2;
-        let gen = this.infOddStream();
+        yield 3;
+        let gen = this.infSixKStream();
         while (true) {
-            const prime = gen.next().value;
-            yield prime;
-            gen = this.filter(gen, prime);
+            const candidate = gen.next().value;
+            if (this.isPrime(candidate, primes)) yield candidate;
         }
     }
 
-    static* infOddStream(i = 1) {
-        while (true) yield i = i + 2;
+    static* infSixKStream() {
+        let k = 1;
+        while (true) {
+            yield k += 4;
+            yield k += 2;
+        }
     }
 
-    static* filter(generator, prime) {
-        for (const n of generator) if (n % prime !== 0) yield n;
+    static isPrime(candidate, primes) {
+        for (let i = 0; primes[i] <= Math.sqrt(candidate); i++) {
+            if (candidate % primes[i] === 0) return false;
+        }
+        primes.push(candidate);
+        return true;
     }
 }
 
+
+
+const {performance} = require('perf_hooks');
+let t0 = performance.now();
 gen = Primes.stream();
-for (let j = 0; j < 100; j++) {
-    console.log(gen.next().value);
+for (let j = 0; j < 1000000; j++) {
+    gen.next().value;
+    // console.log(gen.next().value);
 }
+let t1 = performance.now();
+console.log(`${(t1-t0).toFixed(2)} ms`)
